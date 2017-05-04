a13af1eb1f6994173845955d2551a85170770396
wf4bb <wfmytemp@gmail.com>
2017-05-04 14:42:23 +0800

    add the code to request permission at the runtime

--- app/src/main/java/fl/wf/universalmemorizingassistant/MainActivity.java
+++ app/src/main/java/fl/wf/universalmemorizingassistant/MainActivity.java
@@ -1,13 +1,81 @@
 package fl.wf.universalmemorizingassistant;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "WFMainActivity";
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 4289;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (DataChecker.isExternalStorageWritable()) {
+            Log.d(TAG, "onCreate: T");
+        } else {
+            Log.d(TAG, "onCreate: F");
+        }
+        getRuntimePermission();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    void getRuntimePermission() {
+        Log.d(TAG, "getRuntimePermission: 1");
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "getRuntimePermission: 2");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Log.d(TAG, "getRuntimePermission: 3");
+                new AlertDialog.Builder(this)
+                        .setTitle("TitleHere")
+                        .setMessage("Need Permission")
+                        .setPositiveButton("OK", null)
+                        .show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            } else {
+                Log.d(TAG, "getRuntimePermission: 4");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            }
+        } else {
+            Log.d(TAG, "getRuntimePermission: 5");
+            finish();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onRequestPermissionsResult: 6");
+                } else {
+                    Log.d(TAG, "onRequestPermissionsResult: 7");
+                    Toast.makeText(this, "Need Permission", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                return;
+            }
+        }
     }
 }
