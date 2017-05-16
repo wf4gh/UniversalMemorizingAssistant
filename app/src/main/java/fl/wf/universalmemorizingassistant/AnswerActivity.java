4621161cb4a99d1a424af24e2ee8932d5207a384
wf4bb <wfmytemp@gmail.com>
2017-05-16 08:39:24 +0800

    add basic button logic in answerActivity

--- app/src/main/java/fl/wf/universalmemorizingassistant/AnswerActivity.java
+++ app/src/main/java/fl/wf/universalmemorizingassistant/AnswerActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AnswerActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class AnswerActivity extends AppCompatActivity {
 
     Button buttonYes;
     Button buttonNo;
+    TextView answerHintTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +21,32 @@ public class AnswerActivity extends AppCompatActivity {
 
         buttonYes = (Button) findViewById(R.id.bt_ans_yes);
         buttonNo = (Button) findViewById(R.id.bt_ans_no);
+        answerHintTextView = (TextView) findViewById(R.id.tv_ans_hint);
 
-        // TODO: 2017/5/15 find a chart api,add it
-        // TODO: 2017/5/15 add a "too easy" button
+        // TODO: 2017/5/15 (IUV) find a chart api,add it
+        // TODO: 2017/5/15 (IUV) add a "too easy" button
     }
 
     public void onYesClicked(View view) {
         buttonYes.setVisibility(View.INVISIBLE);
-        answerShowed=false;
+        buttonNo.setText("TOSEE");
+        answerHintTextView.setVisibility(View.INVISIBLE);
+        answerShowed = false;
         // TODO: 2017/5/15 logic need to add here
     }
 
     public void onNoClicked(View view) {
-        if(!answerShowed){
+        if (!answerShowed) {
             buttonYes.setVisibility(View.VISIBLE);
-        }else{
-
+            buttonNo.setText("NO");
+            answerHintTextView.setVisibility(View.VISIBLE);
+            answerShowed = true;
+        } else {
+            buttonYes.setVisibility(View.INVISIBLE);
+            buttonNo.setText("TOSEE");
+            answerHintTextView.setVisibility(View.INVISIBLE);
+            answerShowed = false;
+            // TODO: 2017/5/16 logic add here
         }
     }
 }
