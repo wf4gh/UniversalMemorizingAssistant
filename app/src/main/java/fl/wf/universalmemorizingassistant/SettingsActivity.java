6bf4bc06d8d30bc5092d6e671050ec94a28f3749
wf4bb <wfmytemp@gmail.com>
2017-05-23 10:06:07 +0800

    finished delete book

--- app/src/main/java/fl/wf/universalmemorizingassistant/SettingsActivity.java
+++ app/src/main/java/fl/wf/universalmemorizingassistant/SettingsActivity.java
@@ -1,15 +1,22 @@
 package fl.wf.universalmemorizingassistant;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.util.Objects;
 
+import static android.R.attr.name;
 import static android.os.Environment.getExternalStorageDirectory;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -65,5 +72,55 @@ public class SettingsActivity extends AppCompatActivity {
         int position = booksListView.getCheckedItemPosition();
         setPresentBook(bookNames[position]);
         updateBookNamesAndUI();
+    }
+
+    public void onAddBookClicked(final View view) {
+        final View addBookView = getLayoutInflater().inflate(R.layout.dialog_add_book, null);
+        new AlertDialog.Builder(this)
+                .setTitle("Add a new book")
+                .setView(addBookView)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText addBookEditText = (EditText) addBookView.findViewById(R.id.et_dialog_add_book);
+                        String name = addBookEditText.getText().toString();
+                        if (!name.equals("")) {
+                            // TODO: 2017/5/23 create a new xls here
+                            updateBookNamesAndUI();
+                        } else {
+                            Toast.makeText(SettingsActivity.this, "Enter the name of book", Toast.LENGTH_SHORT).show();
+                            // TODO: 2017/5/23  i'm not sure if this FINAL VIEW will cause any problems...
+                            onAddBookClicked(view);
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
+    }
+
+    public void onDeleteBookClicked(View view) {
+        if (booksListView.getCheckedItemPosition() == -1) {
+            Toast.makeText(this, "Choose a book first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle("Delete Confirmation")
+                .setMessage("Sure to delete?")
+                .setPositiveButton("Sure,delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        File fileToDelete = bookFiles[booksListView.getCheckedItemPosition()];
+                        boolean deleted = fileToDelete.delete();
+
+                        if (!deleted)
+                            Toast.makeText(SettingsActivity.this, "Delete Failed", Toast.LENGTH_SHORT).show();
+                        else
+                            updateBookNamesAndUI();
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
     }
 }
