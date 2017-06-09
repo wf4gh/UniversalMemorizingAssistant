f01713c67361667f47945f4431beb3434b3991cd
wf4bb <wfmytemp@gmail.com>
2017-06-09 10:20:05 +0800

    remove draft area in answer activity

--- app/src/main/java/fl/wf/universalmemorizingassistant/AnswerActivity.java
+++ app/src/main/java/fl/wf/universalmemorizingassistant/AnswerActivity.java
@@ -7,26 +7,21 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.sl.draw.geom.Context;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static android.os.Environment.getExternalStorageDirectory;
 
 public class AnswerActivity extends AppCompatActivity {
-    private static final String TAG = "FLWFAnswerActivity";
+//    private static final String TAG = "FLWFAnswerActivity";
 
     boolean answerShowed = false;
     int answerState = 0;
@@ -43,7 +38,7 @@ public class AnswerActivity extends AppCompatActivity {
     Button noButton;
     TextView hintTextView;
     TextView answerTextView;
-    EditText answerEditText;
+//    EditText answerEditText;
     Intent settingsActivityIntent;
     TextView leftTimesTextView;
 
@@ -66,7 +61,7 @@ public class AnswerActivity extends AppCompatActivity {
         yesButton = (Button) findViewById(R.id.bt_ans_yes);
         noButton = (Button) findViewById(R.id.bt_ans_no);
         hintTextView = (TextView) findViewById(R.id.tv_ans_hint);
-        answerEditText = (EditText) findViewById(R.id.et_ans_ans);
+//        answerEditText = (EditText) findViewById(R.id.et_ans_ans);
         answerTextView = (TextView) findViewById(R.id.tv_ans_answer);
         leftTimesTextView = (TextView) findViewById(R.id.tv_ans_left_times);
     }
@@ -227,7 +222,7 @@ public class AnswerActivity extends AppCompatActivity {
         noButton.setText(getString(R.string.button_remember_not));
         answerTextView.setVisibility(View.VISIBLE);
         answerShowed = true;
-        answerEditText.setEnabled(false);
+//        answerEditText.setEnabled(false);
     }
 
     void UIShowNext() {
@@ -235,8 +230,8 @@ public class AnswerActivity extends AppCompatActivity {
         noButton.setText(getString(R.string.button_show_answer));
         answerTextView.setVisibility(View.INVISIBLE);
         answerShowed = false;
-        answerEditText.setText("");
-        answerEditText.setEnabled(true);
+//        answerEditText.setText("");
+//        answerEditText.setEnabled(true);
     }
 
     String getPresentBook() {
